a46d5f4f2e44511bc5c3bad9a8d51ba1b8929044
NachoGParks <115549632+NachoGParks@users.noreply.github.com>
2022-12-23 20:31:43 -0300

    V 0.0.31
    Reorganizacion ppal de scripts y prefab

--- CH-FINAL/Assets/Scripts/Enemy/EnemyAnimation.cs
+++ CH-FINAL/Assets/Scripts/Enemy/EnemyAnimation.cs
@@ -4,27 +4,16 @@ public class EnemyAnimation : MonoBehaviour
 {
     private Animator animator;
     private Vector3 movementDirection;
-    private GameObject target;
-    private float dist;
-    public int damageAmount = 15;
-	private float timeToShoot = 3f;
-	private float timeToShootLeft;
-    public EnemyMovementTAG varGral;
 
     // Start is called before the first frame update
     void Start()
-    {
-        ResetTimer();
-        animator = GetComponent<Animator>();
-        target = GameObject.FindWithTag("Player");
+    {        
+        animator = GetComponent<Animator>();        
     }
 
     // Update is called once per frame
     void Update()
-    {
-        CheckDistance();
-        Temporizador();      
-
+    { 
         if (transform.hasChanged)
         {            
             animator.SetBool("isRunning", true);
@@ -35,32 +24,4 @@ public class EnemyAnimation : MonoBehaviour
             animator.SetBool("isRunning", false); 
         }
     }
-
-    void CheckDistance()
-    {
-        dist = Vector3.Distance(target.transform.position, transform.position);
-    }
-
-    private void ResetTimer()
-	{
-		timeToShootLeft = timeToShoot;
-	}
-
-    private void Shoot()
-    {
-        PlayerManager.TakeDamage(damageAmount);
-        Debug.Log("Damage taken: " + damageAmount);
-    }
-
-    private void Temporizador()
-	{
-		if(varGral.dead != true)
-        {
-            timeToShootLeft -= Time.deltaTime;
-            if(timeToShootLeft <= 0 && dist <= 1.5) {
-                Shoot();
-                ResetTimer();					
-            }
-        }
-	}
 }
---
+++ CH-FINAL/Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class EnemyManager : MonoBehaviour
+
+{   //VARIABLES
+
+    //Stats
+    public NavMeshAgent agent;
+    public float demonHP = 90f;    
+    public bool demonDead = false;
+
+    //Ataque
+    private GameObject target;    
+    public PlayerManager playerManager;
+    public float demonDamage = 10f;
+    public float distToPlayer;
+    public float timeToShoot = 3f;
+    public float timeToShootLeft = 0f;
+
+    //Defensa
+    public float radius = 1f;
+    public PlayerGun playerGUN;
+
+
+    //FUNCIONES
+
+    //Movimiento
+    public void demonMovement()
+    {
+        if (agent != null)
+        {
+            agent.SetDestination(target.transform.position);
+        }
+    }
+    public void CheckDistance()
+    {
+        distToPlayer = Vector3.Distance(target.transform.position, transform.position);
+    }
+
+    //Ataque
+
+
+    public void Attack()
+    {
+        if (!demonDead)
+        {
+            timeToShootLeft -= Time.deltaTime;
+            if (timeToShootLeft <= 0f && distToPlayer <= 1.5f)
+            {                
+                target.GetComponent<PlayerManager>().playerHP -= 10f;
+
+                timeToShootLeft = timeToShoot;
+                Debug.Log("Attacked");
+            }
+        }
+    }
+
+    //Daño recibido
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (demonDead != true)
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+            foreach (Collider nearbyObject in colliders)
+            {
+                if (nearbyObject.tag == "Impact")
+                {
+                    demonHP -= playerGUN.ShotDamage;
+                    Debug.Log("Enemy HP: " + demonHP);
+
+                    if (demonHP <= 0)
+                    {
+                        Debug.Log("Enemigo Abatido");
+                        demonDead = true;
+                        InGameHUD.dead_Zombies++;
+                    }
+                }
+            }
+        }
+    }
+
+    //Muerte
+    public void enemyDies()
+    {
+        if (demonDead)
+        {
+            Destroy(agent);
+            //insertar animacion de muerte aca!!!!!!!!!!!!!!!!!!!!!!!!!
+        }
+    }
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");        
+    }
+
+    void Update()
+    {        
+        CheckDistance();
+        Attack();
+        demonMovement();        
+        enemyDies();        
+    }
+}
