e7e3fce015a42cb5a5c36c35ed2463330472e264
Francisco Guilman <80010179+reneghar95@users.noreply.github.com>
2022-12-25 21:40:34 -0300

    V ALFALFA
    LISTOSSDASNkfgnAPSNKPFNASPFNÑASFMKPASFNLASFNASKPFNPAKSNASKLÑFGNÑASLGNASKLÑNAK¨FA

--- CH-FINAL/Assets/Scripts/UI/InGameHUD.cs
+++ CH-FINAL/Assets/Scripts/UI/InGameHUD.cs
@@ -1,9 +1,12 @@
+using System.Diagnostics.Tracing;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class InGameHUD : MonoBehaviour
 {
     #region public variables
     public Text KillCount;
+    public Text FoundTags;
     [SerializeField]
     public static int dead_Zombies = 0;
 
@@ -15,13 +18,16 @@ public class InGameHUD : MonoBehaviour
     #endregion
     public PlayerManager playerManager;
 
-
     void Update()
     {
         KillCount.text = "ENEMIES KILLED = " + dead_Zombies.ToString();;
         HUDplayerHP = playerManager.playerHP;
 
         healthBar.fillAmount = HUDplayerHP / MaxHP;
+
+        FoundTags.text = "FOUND TAGS = " + playerManager.TagsFound.ToString() + " of 6"; ;
+
+        
     }
 
 
